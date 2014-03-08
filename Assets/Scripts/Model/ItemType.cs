8ce5831d30f8e3a96134fa4feb19fc8432a7dcb1
qrobert <quentin.robert1@uqac.ca>
2014-03-08 16:41:43 -0500

    1er jet stats aux items

--- Assets/Scripts/Model/ItemType.cs
+++ Assets/Scripts/Model/ItemType.cs
@@ -51,25 +51,25 @@ public class ItemType
 
     // --- DEFINE GAME ITEMS HERE ---
 
-    public static readonly ItemType WeaponT0 = new ItemType("Journal roulé", "todo", Slots.Weapon, 0, Stats.Zero);
-    public static readonly ItemType WeaponT1 = new ItemType("Tuyaux plomberie", "todo", Slots.Weapon, 1, Stats.Zero);
-    public static readonly ItemType WeaponT2 = new ItemType("Sledgehammer", "todo", Slots.Weapon, 2, Stats.Zero);
-    public static readonly ItemType WeaponT3 = new ItemType("Lightsaber", "todo", Slots.Weapon, 3, Stats.Zero);
+    public static readonly ItemType WeaponT0 = new ItemType("Journal roulé", "todo", Slots.Weapon, 0, new Stats(0, 3, 0f, 0f));
+    public static readonly ItemType WeaponT1 = new ItemType("Tuyaux plomberie", "todo", Slots.Weapon, 1, new Stats(0, 7, 0f, 0f));
+    public static readonly ItemType WeaponT2 = new ItemType("Sledgehammer", "todo", Slots.Weapon, 2, new Stats(0, 11, 0f, 0f));
+    public static readonly ItemType WeaponT3 = new ItemType("Lightsaber", "todo", Slots.Weapon, 3, new Stats(0, 15, 0f, 0f));
 
-    public static readonly ItemType ArmorT0 = new ItemType("Barre de censure", "todo", Slots.Armor, 0, Stats.Zero);
-    public static readonly ItemType ArmorT1 = new ItemType("Veste pare-balle", "todo", Slots.Armor, 1, Stats.Zero);
-    public static readonly ItemType ArmorT2 = new ItemType("Riot gear", "todo", Slots.Armor, 2, Stats.Zero);
-    public static readonly ItemType ArmorT3 = new ItemType("Power armor", "todo", Slots.Armor, 3, Stats.Zero);
+    public static readonly ItemType ArmorT0 = new ItemType("Barre de censure", "todo", Slots.Armor, 0, new Stats(10, 0, 0f, 0f));
+    public static readonly ItemType ArmorT1 = new ItemType("Veste pare-balle", "todo", Slots.Armor, 1, new Stats(30, 0, 0f, 0f));
+    public static readonly ItemType ArmorT2 = new ItemType("Riot gear", "todo", Slots.Armor, 2, new Stats(60, 0, 0f, 0f));
+    public static readonly ItemType ArmorT3 = new ItemType("Power armor", "todo", Slots.Armor, 3, new Stats(100, 0, 0f, 0f));
 
-    public static readonly ItemType ToolT0 = new ItemType("Main sale", "todo", Slots.Tool, 0, Stats.Zero);
-    public static readonly ItemType ToolT1 = new ItemType("Pelle", "todo", Slots.Tool, 1, Stats.Zero);
-    public static readonly ItemType ToolT2 = new ItemType("Super aimant", "todo", Slots.Tool, 2, Stats.Zero);
-    public static readonly ItemType ToolT3 = new ItemType("Gravity gun", "todo", Slots.Tool, 3, Stats.Zero);
+    public static readonly ItemType ToolT0 = new ItemType("Main sale", "todo", Slots.Tool, 0, new Stats(0, 0, 0.1f, 0f));
+    public static readonly ItemType ToolT1 = new ItemType("Pelle", "todo", Slots.Tool, 1, new Stats(0, 0, 0.25f, 0f));
+    public static readonly ItemType ToolT2 = new ItemType("Super aimant", "todo", Slots.Tool, 2, new Stats(0, 0, 0.50f, 0f));
+    public static readonly ItemType ToolT3 = new ItemType("Gravity gun", "todo", Slots.Tool, 3, new Stats(0, 0, 1.0f, 0f));
 
-    public static readonly ItemType BootsT0 = new ItemType("Pieds sales", "todo", Slots.Boots, 0, Stats.Zero);
-    public static readonly ItemType BootsT1 = new ItemType("Soulier", "todo", Slots.Boots, 1, Stats.Zero);
-    public static readonly ItemType BootsT2 = new ItemType("Soulier qui allume", "todo", Slots.Boots, 2, Stats.Zero);
-    public static readonly ItemType BootsT3 = new ItemType("Bacorn slippers", "todo", Slots.Boots, 3, Stats.Zero);
+    public static readonly ItemType BootsT0 = new ItemType("Pieds sales", "todo", Slots.Boots, 0, new Stats(0, 0, 0f, 0.2f));
+    public static readonly ItemType BootsT1 = new ItemType("Soulier", "todo", Slots.Boots, 1, new Stats(0, 0, 0f, 0.4f));
+    public static readonly ItemType BootsT2 = new ItemType("Soulier qui allume", "todo", Slots.Boots, 2, new Stats(0, 0, 0f, 0.6f));
+    public static readonly ItemType BootsT3 = new ItemType("Bacorn slippers", "todo", Slots.Boots, 3, new Stats(0, 0, 0f, 0.8f));
 
     /// <summary>
     /// Available items ingame.
