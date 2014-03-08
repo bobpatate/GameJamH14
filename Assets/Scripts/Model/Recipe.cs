e0296bc35ff6a9371b5ed095ae9b24bb5880502f
qrobert <quentin.robert1@uqac.ca>
2014-03-08 13:27:43 -0500

    Algo choix items
    Avec de la magie vaudou

--- Assets/Scripts/Model/Recipe.cs
+++ Assets/Scripts/Model/Recipe.cs
@@ -4,31 +4,28 @@
     /// Slot built by this recipe.
     /// </summary>
     public Slots Slot { get; private set; }
-	public int preferredMetal;
-	public int preferredTextile;
-	public int preferredElectronic;
-    /// <summary>
-    /// Cost of this recipe.
-    /// </summary>
-    //public Ressource[] Cost { get; private set; }
 
-    private Recipe(Slots slot, int Metal, int Textile, int Electronic)
+    public float MetalRatio { get; private set; }
+    public float TextileRatio { get; private set; }
+    public float ElectronicRatio { get; private set; }
+
+    private Recipe(Slots slot, float metal, float textile, float electronic)
     {
         Slot = slot;
-		preferredMetal=Metal;
-		preferredTextile=Textile;
-		preferredElectronic=Electronic;
+        MetalRatio = metal;
+        TextileRatio = textile;
+        ElectronicRatio = electronic;
     }
 
     // --- DEFINE GAME RECIPES HERE ---
 
-//    public static readonly Recipe Weapon = new Recipe(Slots.Weapon /*, new Truc(5, 2, 1)*/);
-//    public static readonly Recipe Armor = new Recipe(Slots.Armor /*, new Truc(2, 1, 5)*/);
-//    public static readonly Recipe Tool = new Recipe(Slots.Tool /*, new Truc(1, 5, 2)*/);
-//    public static readonly Recipe Boots = new Recipe(Slots.Boots /*, new Truc(3, 3, 3)*/);
+    public static readonly Recipe Weapon = new Recipe(Slots.Weapon, 0.5f, 0.2f, 0.1f);
+    public static readonly Recipe Armor = new Recipe(Slots.Armor, 0.2f, 0.1f, 0.5f);
+    public static readonly Recipe Tool = new Recipe(Slots.Tool, 0.1f, 0.5f, 0.2f);
+    public static readonly Recipe Boots = new Recipe(Slots.Boots, 0.3f, 0.3f, 0.3f);
 
     /// <summary>
     /// Possible recipes ingame.
     /// </summary>
-//    public static readonly Recipe[] List = { Weapon, Armor, Tool, Boots };
+    public static readonly Recipe[] List = { Weapon, Armor, Tool, Boots };
 }
---
+++ Assets/Scripts/Test/ItemBuilderTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemBuilderTest : MonoBehaviour {
+    void Start()
+    {
+        Test();
+    }
+
+    private static Ressource metalTestT0 = new Ressource() { nom = "metal", tier = 0 };
+    private static Ressource metalTestT1 = new Ressource() { nom = "metal", tier = 1 };
+    private static Ressource metalTestT2 = new Ressource() { nom = "metal", tier = 2 };
+    private static Ressource metalTestT3 = new Ressource() { nom = "metal", tier = 3 };
+    private static Ressource electronicTestT0 = new Ressource() { nom = "electronic", tier = 0 };
+    private static Ressource electronicTestT1 = new Ressource() { nom = "electronic", tier = 1 };
+    private static Ressource electronicTestT2 = new Ressource() { nom = "electronic", tier = 2 };
+    private static Ressource electronicTestT3 = new Ressource() { nom = "electronic", tier = 3 };
+    private static Ressource textileTestT0 = new Ressource() { nom = "textile", tier = 0 };
+    private static Ressource textileTestT1 = new Ressource() { nom = "textile", tier = 1 };
+    private static Ressource textileTestT2 = new Ressource() { nom = "textile", tier = 2 };
+    private static Ressource textileTestT3 = new Ressource() { nom = "textile", tier = 3 };
+
+    private static Ressource[] inventoryTest1 = new Ressource[] {
+       metalTestT2,
+       metalTestT0,
+       metalTestT2,
+       metalTestT1,
+       metalTestT1,
+       electronicTestT3,
+       textileTestT1,
+    };
+
+    public static void Test()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            ItemType type = ItemBuilder.Pick(inventoryTest1);
+
+            Debug.Log(string.Format("{0}. Chosen item: {1}", i, type.Name));
+        }
+    }
+}
