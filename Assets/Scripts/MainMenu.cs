d55eed7efcf39d382097eb9ec0338bae84139e0f
Gab <ghebert10@gmail.com>
2014-03-09 03:31:15 -0400

    main fuckfuck

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnGUI () {
+		GUI.backgroundColor = Color.yellow;
+		if (GUI.Button (new Rect (Screen.width/2,Screen.height,150,70), "PLAY")) {
+			Application.LoadLevel("MainGame");
+		}
+	}
+}
