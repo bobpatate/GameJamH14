381942d63492f19d11dbb12d22cd63a4c4d18ea5
Gab <ghebert10@gmail.com>
2014-03-09 07:21:51 -0400

    menufuck avec bouton fuck

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour {
 
 	void OnGUI () {
 		GUI.backgroundColor = Color.yellow;
-		if (GUI.Button (new Rect (Screen.width/2,Screen.height,150,70), "PLAY")) {
+		if (GUI.Button (new Rect (Screen.width*0.31f, Screen.height*0.6f , Screen.width/6, Screen.height/7), "PLAY")) {
 			Application.LoadLevel("MainGame");
 		}
 	}
