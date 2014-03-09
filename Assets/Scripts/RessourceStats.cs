61d59e2cf7c4939de4255277a7b64402dce95dd0
Gab <ghebert10@gmail.com>
2014-03-09 09:36:45 -0400

    ultimate ressource collecting 2000

--- Assets/Scripts/RessourceStats.cs
+++ Assets/Scripts/RessourceStats.cs
@@ -5,29 +5,46 @@ public class RessourceStats : MonoBehaviour {
 	public float collectingTime;
 	public int tier;
 
-	bool harvested;
-	float respawnDelay = 30.0f;
+	public bool harvested;
+	float respawnDelay = 25.0f;
 	float harvestedAtTime;
 	float respawnTime;
 
+	private Shader shaderHarvested;
+	private Shader shader;
+
 	// Use this for initialization
 	void Start () {
-	
+		shaderHarvested = Shader.Find("Bumped Diffuse");
+		shader = Shader.Find("Shader Forge/shader_corrosif");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(harvested){
+			if(Time.time >= respawnTime){
+				harvested = false;
 
+				if(name == "Textile")
+					gameObject.transform.FindChild("SM_clothes").renderer.material.shader = shader;
+				else if(name == "Metal")
+					gameObject.transform.FindChild("SM_metal").renderer.material.shader = shader;
+				else if(name == "Electronique")
+					gameObject.transform.FindChild("SM_tv").renderer.material.shader = shader;
+			}
 		}
 	}
 
 	public void Harvested(){
 		harvested = true;
 		harvestedAtTime = Time.time;
+		respawnTime = harvestedAtTime + respawnDelay;
 
-		//transform.FindChild("Collectile_Trigger").gameObject.GetComponent<SphereCollider>().enabled = false;
-		//transform.FindChild("Collectile_Trigger").gameObject.GetComponent<MeshRenderer>().enabled = false;
-		//transform.FindChild("SM_Clothes").GetComponent<MeshRenderer>().enabled = false;
+		if(name == "Textile")
+			gameObject.transform.FindChild("SM_clothes").renderer.material.shader = shaderHarvested;
+		else if(name == "Metal")
+			gameObject.transform.FindChild("SM_metal").renderer.material.shader = shaderHarvested;
+		else if(name == "Electronique")
+			gameObject.transform.FindChild("SM_tv").renderer.material.shader = shaderHarvested;
 	}
 }
