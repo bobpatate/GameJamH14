1c5333e06826aff01f8ee9e888d72c24eff9eed2
Drackhyo <drackhyo@gmail.com>
2014-03-09 12:19:36 -0400

    menumenumenumenumenumenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour {
 
 	void OnGUI () {
 		GUI.backgroundColor = Color.yellow;
-		if (GUI.Button (new Rect (Screen.width/2,Screen.height,150,70), "PLAY")) {
+		if (GUI.Button (new Rect (Screen.width*0.29f, Screen.height*0.58f, Screen.width*0.2f, Screen.height*0.15f), "PLAY")) {
 			Application.LoadLevel("MainGame");
 		}
 	}
